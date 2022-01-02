6065a157e091c8a81bbd13f95f2243916223ea4e
SHiuN26 <justodme26@gmail.com>
2022-01-03 01:50:01 +0800

    1

--- yt.js
+++ yt.js
@@ -18,6 +18,14 @@ $(document).ready(function () {
         }
     });
 
+function detecat_cat(cat_id,x){
+    var catplace=$(cat_id).offset().left+$(cat_id).width()/2;
+    if(Math.abs(x-catplace)<80)
+        $(cat_id).css("bottom","0px")
+    else
+        $(cat_id).css("bottom","-50px")
+}
+
     $(window).mousemove(function (evt) { 
         // values: e.clientX, e.clientY, e.pageX, e.pageY
         var pagex=evt.pageX;
@@ -26,19 +34,41 @@ $(document).ready(function () {
         var x=pagex-$("#section_about").offset().left;
         var y=pagey-$("#section_about").offset().top;
         // console.log(x+","+y);
-        console.log(pagey);
+        // console.log(pagey);
         // console.log(pagey,y);
         // console.log($("#section_about").offset().top);
         // console.log($("#section_about").offset().left);
-        if(y>0||y>$("section_about").outerHeight())
-            $("#cross").css(opacity,0)
+        if(y<0||
+            y>$("section_about").outerHeight())
+            $("#cross").css("opacity",0);
         else
-            $("#cross").css(opacity,0)
+            $("#cross").css("opacity",1);
 
+        
+        var catplace=$("#cat").offset().left+$("#cat").width()/2;
+        var cattop=$("#cat").offset().top;
+        var img_url="https://awiclass.monoame.com/catpic/";
+        
+        if(pagex<catplace-50)
+            $("#cat").attr("src",img_url+"cat_left.png")
+        else if(pagex>catplace+50)
+            $("#cat").attr("src",img_url+"cat_right.png")
+        else
+            $("#cat").attr("src",img_url+"cat_top.png")
+
+        if(pagex<catplace-50 && pagey<cattop)
+            $("#cat").attr("src",img_url+"cat_lefttop.png")
+        if(pagex>catplace+50 && pagey<cattop)
+            $("#cat").attr("src",img_url+"cat_righttop.png")
+            
 
         $("#cross").css("left",x+"px");
         $("#cross").css("top",y+"px");
+        
 
+        detecat_cat("#cat_yellow",pagex);
+        detecat_cat("#cat_blue",pagex);
+        detecat_cat("#cat_grey",pagex);
 
         $(".mountain").css("transform","translateX("+(pagex/-20+50)+"px)")
         $(".r1text").css("transform","translateX("+(y/-5)+"px)")
